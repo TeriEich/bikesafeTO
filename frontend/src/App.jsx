85bf82b244edcd750716bcecbb325272b83c94f3
vagrant <tom.w.j.sanderson@gmail.com>
2019-01-28 02:34:44 +0000

    added a single marker onto the map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import MainMap from './Map';
 
 export default class App extends React.Component {
 
-// *** need to use axios to send data to database ***
+
   render() {
     return (
       <div className="map-container">
         <MainMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAJOfXbTHxSPOxeOJgA0c2MFKJ6aMNy5eI&v=3.exp&libraries=geometry,drawing,places`}
+          // googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAJOfXbTHxSPOxeOJgA0c2MFKJ6aMNy5eI&v=3.exp&libraries=geometry,drawing,places`}
           center={{lat: 43.6532, lng: -79.3832}}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `600px`, width: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />} />
+          mapElement={<div style={{ height: `100%` }} />}
+          />
       </div>
     );
   }
 }
-
--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -4,19 +4,15 @@ import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 class MainMap extends Component {
 
   render() {
-    //allows for an empty array instead of returning undefined if no markers found
-    const markers = this.props.markers || []
 
     return (
       // GoogleMap must have perameters defaultZoom + defaultCenter
       <GoogleMap
-        defaultZoom={10}
+        defaultZoom={14}
         defaultCenter={this.props.center}>
-        {markers.map((marker, index) => (
-            <Marker {...marker} />
-            )
-        )}
-
+        <Marker
+        position={{ lat: 43.6442, lng: -79.4022 }}
+        />
       </GoogleMap>
 
     )
@@ -25,3 +21,13 @@ class MainMap extends Component {
 
 // react-google-map uses HOC to work, this export syntax is used at the bottom of the file for the sake of clarity
 export default withGoogleMap(MainMap)
+
+
+// // in the render
+// //allows for an empty array instead of returning undefined if no markers found
+// const markers = this.props.markers || []
+// // in the return
+// {markers.map((marker, index) => (
+//             <Marker {...marker} />
+//             )
+//         )}
